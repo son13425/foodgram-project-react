f9da8f218536f1aff0ddbe24f196d78643aaf472
Оксана Широкова <son134@yandex.ru>
2022-10-18 13:54:14 +0300

    deploy11

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -142,16 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'build/static')
-#]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
-#MEDIAFILES_DIRS = [
-#   os.path.join(BASE_DIR, 'build/media')
-#]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
