e837dde516b457c735a8c7479b6b3f4aa85938c5
Оксана Широкова <son134@yandex.ru>
2022-11-13 12:27:31 +0300

    ssl

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -15,7 +15,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'DEFAULT')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default='False')
+
 
 ALLOWED_HOSTS = ["*"]
 
